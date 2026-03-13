62be9258f056041ffd06c8911a28d1af9252a27c
Lubeli <144096691+Lubeli@users.noreply.github.com>
2026-03-13 13:12:57 +0900

    Update 4-3.c
    Fix prime number check logic and initialize count.

--- code/4-3.c
+++ code/4-3.c
@@ -2,6 +2,19 @@
 
 int main()
 {
+    int a,count;
+    scanf("%d",&a);
+    for(int i = 2; i<a;i++){
+        if(a%i == 0){
+            count++;
+        }
+    }
+    
+    if(count == 1){
+        printf("true");
+    }else{
+        printf("false");
+    }
 
     return 0;
 }
