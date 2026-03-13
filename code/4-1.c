6d63e901710ee55d71817c63ff07e2fd9d4f2389
Lubeli <144096691+Lubeli@users.noreply.github.com>
2026-03-13 12:04:47 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,6 +2,20 @@
 
 int main()
 {
+    int a,b;
+    char c;
+    scanf("%d", &a);
+    scanf(" %d", &b);
+    scanf(" %c", &c);
+    if( c =='+'){
+        printf("%d", a+b);
+    }else if( c == '-'){
+        printf("%d", a-b);
+    }else if( c == '*'){
+        printf("%d", a*b);
+    }else if( c == '/'){
+        printf("%d", a/b);
+    }
 
     return 0;
 }
